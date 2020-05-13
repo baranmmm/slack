4b0534a23b726039384eb2644f433e5fc2714353
baranmmm <59487546+baranmmm@users.noreply.github.com>
2020-05-13 19:38:20 +0100

    Update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -4,6 +4,8 @@ public class Login {
 
     public static void main(String[] args) {
         //Slack users can login successfully
+        //Usernames and passwords were changed successfully
+        
 
     }
 }
