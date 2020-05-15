88135d951b1c781ba2beba06026032598b75bf6d
baranmmm <m.barann84@gmail.com>
2020-05-15 16:07:29 +0100

    add Emoji.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,5 +3,10 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
 
+        int x=0;
+        if(x==0){
+            System.out.println("equal");
+        }
+
     }
 }
